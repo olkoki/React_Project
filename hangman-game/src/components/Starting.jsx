09f79aeb6320d35a3c914603f461d8a963160eb8
Angela Rakic <andjela.nikolic.arh@gmail.com>
2025-11-27 14:12:37 +0100

    Started with the starting page

---
+++ hangman-game/src/components/Starting.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function StartingWindow=()=> {
+    return (
+<div 
+style=  {{ width: 48%;
+  height: 30vh;
+  backgroundColor="#FFFAA0"
+}}>
+  <h2>This is Hangman Game</h2>
+  <p>Created by Alexandra, Matea, Mariana and Angela</p>
+  <p>With ReDi School for Digital Integration</p>
+  <button onClick={} style={}>Play Game</button>
+</div>
+    )
+};
+
+
+export default StartingWindow
